5312cba47ba53f4fb2ff80e66a4989089e2627f1
sw931110 <sw931110@naver.com>
2019-10-04 16:52:29 +0900

    -databinding 그래들 수정, splash화면 추가

--- MyCalender/app/src/main/java/com/siwon/mycalender/ui/intro/Splash.kt
+++ MyCalender/app/src/main/java/com/siwon/mycalender/ui/intro/Splash.kt
@@ -1,4 +1,30 @@
 package com.siwon.mycalender.ui.intro
 
-class Splash {
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.siwon.mycalender.MainActivity
+import com.siwon.mycalender.R
+import com.siwon.mycalender.databinding.SplashBinding
+
+class Splash : AppCompatActivity() {
+
+    lateinit var bb: SplashBinding
+    val DURATION:Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bb = DataBindingUtil.setContentView(this, R.layout.splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DURATION)
+    }
+
+    override fun onBackPressed() {
+    }
 }
